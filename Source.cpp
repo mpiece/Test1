657a697583c4778c9a867fb344301a93d3c3229f
unknown <keli2614@T216.win.dsv.su.se>
2013-12-06 16:47:04 +0100

    gjort en print funktion

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void print(){
+	cout << "Hej!";
+}
+
 int main(int argc, char *argv){
 	cout << "hello world!"<< endl;
 	cout << " test 1" <<endl;
+
 }
