89151c3d4e2a8c3466077618b355dc54a0fa47af
unknown <dato5208@T215.win.dsv.su.se>
2013-12-06 16:57:35 +0100

    revert to made a println function

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void print(){
-	cout << "Hej!";
-}
-
 int main(int argc, char *argv){
 	cout << "hello world!"<< endl;
 	cout << " test 1" <<endl;
-	cout << "jag ändrar också här" << endl;
 }
