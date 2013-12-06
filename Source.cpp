482bb37930f6b6ea21e5a1905c00bed308a1217e
unknown <dato5208@T215.win.dsv.su.se>
2013-12-06 17:31:56 +0100

    Revert "fixed"
    This reverts commit ec212ca514800dbf68c7c2728e94224553537e24.

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(int argc, char *argv){
 	cout << "hello world!"<< endl;
 	cout << " test 1" <<endl;
+	cout << "test 2 " << endl;
 	cout << "test 3" << endl;
 	cout << "test 4" << endl;
 
